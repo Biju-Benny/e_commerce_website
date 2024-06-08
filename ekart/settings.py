03995a2ae90afa511043f159b422c7dd91548871
Biju Benny <70836378+Biju-Benny@users.noreply.github.com>
2024-06-08 21:51:49 +0530

    m

--- ekart/settings.py
+++ ekart/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'ecart_files/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS= [
     BASE_DIR/ "static"
 ]
